cc3c14e95a93bcc4999e095493a984f4ac12a34e
Byron Lutz <byronlutz@mail.weber.edu>
2024-08-11 16:10:35 -0600

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Byron Lutz on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
